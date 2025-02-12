8bb2209f05e26f42283c5fe2b6ba3d1ec1dfdf52
Ghost_Toni <41290012+ghosttoni@users.noreply.github.com>
2025-02-12 02:45:19 +0100

    Update ark-sa-custom-start.sh

--- ark-sa-custom-start.sh
+++ ark-sa-custom-start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-.proton/proton "$@" & ARK_PID=$!
-tail --pid=$ARK_PID -c0 -F 2430930/ShooterGame/Saved/Logs/ShooterGame.log
+echo "Starting \"ARK: Survival Ascended\" Server..."
+
+/AMP/ark-sa/.proton/proton "$@" & ARK_PID=$!
+tail --pid=$ARK_PID -c0 -F /AMP/ark-sa/2430930/ShooterGame/Saved/Logs/ShooterGame.log
+
+echo "\"ARK: Survival Ascended\" Server stopped"
